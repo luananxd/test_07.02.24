90f72df8724c28f9abc9c5a5d8688ade717182cf
Andrey Luzan <andrejluzan@gmail.com>
2024-02-09 18:35:46 +0700

    Сделана стилизация превью блока

--- src/components/HomePagePreview.vue
+++ src/components/HomePagePreview.vue
@@ -8,10 +8,72 @@
         <p class="preview__subtitle">класса "монитор-кресло"</p>
       </div>
       <div class="preview__image-wrapper">
-        <img class="preview__image" src="@/assets/img/robots-preview.png" alt="Два робота: синий и оранжевый" width="366" height="320">
+        <picture>
+          <!-- <source srcset="@/assets/img/robots-preview.webp 1x, @/assets/img/robots-preview@2x.webp 2x" type="image/webp"> -->
+          <source srcset="@/assets/img/robots-preview@2x.png 2x" type="image/png">
+          <img class="preview__image" src="@/assets/img/robots-preview.png" alt="Два робота: синий и оранжевый" width="366" height="320">
+        </picture>
       </div>
     </div>
   </section>
 </template>
 
 <script setup></script>
+
+<style lang="scss">
+@import "@/assets/scss/text-style.scss";
+@import "@/assets/scss/colors.scss";
+
+.preview {
+  height: 100vh;
+  padding-top: 192px;
+  padding-bottom: 50px;
+}
+
+.preview__scroll-info {
+  position: absolute;
+  top: 225px;
+  left: -20px;
+  font-size: 12px;
+  line-height: 24px;
+  font-weight: 500;
+  text-transform: uppercase;
+  color: $dull-blue;
+  transform: rotate(-90deg);
+}
+
+.preview__scroll-info::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: -32px;
+  width: 24px;
+  height: 24px;
+  background-image: url("@/assets/img/arrow.svg");
+  transform: rotate(90deg);
+}
+
+.preview__wrapper {
+  display: flex;
+  position: relative;
+}
+
+.preview__title-wrapper {
+  width: 470px;
+  margin-left: 130px;
+  margin-right: auto;
+}
+
+.preview__title {
+  @include h1;
+  margin-top: 24px;
+  margin-bottom: 24px;
+}
+
+.preview__subtitle {
+  @include h3;
+  margin: 0;
+  color: $light-blue;
+}
+
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -16,3 +16,18 @@ import HomePageStock from "@/components/HomePageStock.vue";
 import HomePageInstallation from "@/components/HomePageInstallation.vue";
 
 </script>
+
+<style lang="scss">
+@import "@/assets/scss/text-style.scss";
+@import "@/assets/scss/colors.scss";
+
+.section-number {
+  position: absolute;
+  top: 48px;
+  left: 0;
+  font-size: 12px;
+  line-height: 24px;
+  font-weight: 500;
+  color: $dull-blue;
+}
+</style>
