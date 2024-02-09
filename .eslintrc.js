6a7a4a00563634056aeb0dd171dd7b5c24cb595e
Andrey Luzan <andrejluzan@gmail.com>
2024-02-09 19:50:55 +0700

    Внесены изменения в настройки проекта

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  extends: [
+    "airbnb-base",
+    // "plugin:vue/vue3-recommended",
+    // "eslint:recommended",
+    // "@vue/eslint-config-prettier",
+  ],
+  env: {
+    "vue/setup-compiler-macros": true,
+  },
+};
