cef880d4198a86349acdb7a6a17ffb5b3d3adee5
Andrey Luzan <andrejluzan@gmail.com>
2024-02-09 18:08:24 +0700

    Создан проект

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <AppLayout />
+</template>
+
+<script setup>
+import AppLayout from "@/layout/AppLayout.vue";
+</script>
+
+<style lang="scss">
+@import "@/assets/scss/text-style.scss";
+@import "@/assets/scss/colors.scss";
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+}
+</style>
