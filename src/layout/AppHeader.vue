32111ca059a55e6135b11dfc71548b71f886131a
Andrey Luzan <andrejluzan@gmail.com>
2024-02-09 18:35:20 +0700

    Сделана стилизация макета и хедера

--- src/layout/AppHeader.vue
+++ src/layout/AppHeader.vue
@@ -14,3 +14,51 @@
 </template>
 
 <script setup></script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/text-style.scss";
+@import "@/assets/scss/colors.scss";
+
+.page-header {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  padding: 48px 0;
+}
+
+.page-header__wrapper {
+  display: flex;
+}
+
+.page-header__logo {
+  margin-right: auto;
+}
+.logo__icon {
+  color: $orange;
+}
+
+.page-header__button {
+  display: block;
+  padding: 12px 18px;
+  @include normal-text;
+  color: $white;
+  text-decoration: none;
+  border: 2px solid $orange;
+  border-radius: 60px;
+
+  &:hover {
+    background-color: $red-orange;
+    border-color: $red-orange;
+  }
+
+  &:active {
+    background-color: $orange;
+    border-color: $orange;
+  }
+
+  &:focus-visible {
+    outline: 3px solid $violet;
+  }
+}
+</style>
--- src/layout/AppLayout.vue
+++ src/layout/AppLayout.vue
@@ -9,3 +9,27 @@
 import AppHeader from "@/layout/AppHeader.vue";
 import HomePage from "@/pages/HomePage.vue";
 </script>
+
+<style lang="scss">
+@import "@/assets/scss/colors.scss";
+
+html {
+  scroll-behavior: smooth;
+}
+
+body {
+  margin: 0;
+  background-image: radial-gradient(1800px at top center, #26394d, #212529);
+  font-family: "Montserrat", "Arial", sans-serif;
+  color: $white;
+}
+
+.app-layout {
+  position: relative;
+}
+
+.center-wrapper {
+  width: 1016px;
+  margin: 0 auto;
+}
+</style>
