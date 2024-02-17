cde2d873f8c0afb399948a76cc21f392794e05cd
Andrey Luzan <andrejluzan@gmail.com>
2024-02-17 21:42:00 +0700

    Сделана функция для рендера названия компонентов

---
+++ src/common/helpers.js
@@ -0,0 +1,45 @@
+export const getCoinsDescription = (quantity) => {
+  if (quantity % 10 === 1) {
+    return "монета"
+  }
+  
+  if (quantity % 10 > 1 && quantity % 10 < 5) {
+    return "монеты"
+  }
+  return "монет"
+}
+
+export const getComponentDescription = (componentId, quantity) => {
+  switch(componentId) {
+    case "1":
+      if (quantity % 10 === 1) {
+        return "биорука"
+      }
+      
+      if (quantity % 10 > 1 && quantity % 10 < 5) {
+        return "биоруки"
+      }
+      return "биорук"
+
+    case "2":
+      if (quantity % 10 === 1) {
+        return "микрочип"
+      }
+      
+      if (quantity % 10 > 1 && quantity % 10 < 5) {
+        return "микрочипа"
+      }
+      return "микрочипов"
+
+    
+    case "3":
+    if (quantity % 10 === 1) {
+      return "души"
+    }
+    
+    if (quantity % 10 > 1 && quantity % 10 < 5) {
+      return "души"
+    }
+    return "душ"
+  }
+}
