31c279fc4d9ae2d387b2a5b27b9d69952a3e312f
Andrey Luzan <andrejluzan@gmail.com>
2024-02-17 21:38:35 +0700

    Сделана глобальная регистрация компонентов

---
+++ src/components/UI/index.js
@@ -0,0 +1,23 @@
+import AppButton from './AppButton.vue';
+import AppRadio from './AppRadio.vue';
+import AppCheckbox from './AppCheckbox.vue';
+import AppModal from './AppModal.vue';
+
+export default [
+  {
+    name: 'AppButton',
+    value: AppButton,
+  },
+  {
+    name: 'AppRadio',
+    value: AppRadio,
+  },
+  {
+    name: 'AppCheckbox',
+    value: AppCheckbox,
+  },
+  {
+    name: 'AppModal',
+    value: AppModal,
+  },
+];
--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
-import { createApp } from "vue";
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
-import App from "./App.vue";
-import AppButton from "./components/UI/AppButton.vue";
-import AppRadio from "./components/UI/AppRadio.vue";
-import AppCheckbox from "./components/UI/AppCheckbox.vue";
+import App from './App.vue';
+import globalComponents from './components/UI/index';
 
 const app = createApp(App);
+const pinia = createPinia();
 
-app.component("AppButton", AppButton);
-app.component("AppRadio", AppRadio);
-app.component("AppCheckbox", AppCheckbox);
-app.mount("#app");
+app.use(pinia);
+globalComponents.forEach((component) => {
+  app.component(component.name, component.value);
+});
+app.mount('#app');
