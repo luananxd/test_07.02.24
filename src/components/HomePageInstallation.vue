eff4573c8539d4b077ac4749f55c0084d2a7a42f
Andrey Luzan <andrejluzan@gmail.com>
2024-02-17 21:38:58 +0700

    Сделана логика для конструктора робота

--- src/components/HomePageInstallation.vue
+++ src/components/HomePageInstallation.vue
@@ -1,132 +1,145 @@
 <template>
   <section class="installation section-block">
     <div class="installation__wrapper center-wrapper">
-      <span class="installation__number section-number section-number--installation">05</span>
-        <h2 class="installation__title section-title">Производство</h2>
-        <div class="installation__constructor constructor" id="constructor">
-          <form class="constructor__form" method="POST" action="#">
-            <div class="constructor__body">
-              <div class="constructor__settings">
-                <fieldset class="constructor__parameter parameter">
-                  <legend class="parameter__title">Тип биоробота:</legend>
-                  <div class="parameter__wrapper">
-                    <AppRadio class="parameter__label">FrontEnd</AppRadio>
-                    <AppRadio class="parameter__label">Design</AppRadio>
-                  </div>
-                </fieldset>
-                <fieldset class="constructor__parameter parameter">
-                  <legend class="parameter__title">Стабилизатор:</legend>
-                  <div class="parameter__wrapper">
-                    <AppRadio class="parameter__label">Male</AppRadio>
-                    <AppRadio class="parameter__label">Female</AppRadio>
-                  </div>
-                </fieldset>
-              </div>
-              <fieldset class="constructor__components components">
-                <legend class="visually-hidden">Добавить компоненты для производства</legend>
-                <ul class="components__list">
-                  <li>
-                    <label class="components__item components__item--biohand">
-                      <svg class="components__logo" width="36" height="24">
-                        <use href="@/assets/img/sprite.svg#biohand"></use>
-                      </svg>
-                      <input class="visually-hidden" type="checkbox" name="biohand-1" checked>
-                      <span class="visually-hidden">Вернуть биоруку на склад</span>
-                    </label>
-                  </li>
-                  <li>
-                    <label class="components__item components__item--biohand">
-                      <svg class="components__logo" width="36" height="24">
-                        <use href="@/assets/img/sprite.svg#biohand"></use>
-                      </svg>
-                      <input class="visually-hidden" type="checkbox" name="biohand-2" checked>
-                      <span class="visually-hidden">Вернуть биоруку на склад</span>
-                    </label>
-                  </li>
-                  <li>
-                    <label class="components__item components__item--biohand">
-                      <svg class="components__logo" width="36" height="24">
-                        <use href="@/assets/img/sprite.svg#biohand"></use>
-                      </svg>
-                      <input class="visually-hidden" type="checkbox" name="biohand-3">
-                      <span class="visually-hidden">Добавить биоруку</span>
-                    </label>
-                  </li>
-                  <li>
-                    <label class="components__item components__item--biohand">
-                      <svg class="components__logo" width="36" height="24">
-                        <use href="@/assets/img/sprite.svg#biohand"></use>
-                      </svg>
-                      <input class="visually-hidden" type="checkbox" name="biohand-4" disabled>
-                    </label>
-                  </li>
-                </ul>
-                <ul class="components__list">
-                  <li>
-                    <label class="components__item components__item--chip">
-                      <svg class="components__logo" width="24" height="24">
-                        <use href="@/assets/img/sprite.svg#chip"></use>
-                      </svg>
-                      <input class="visually-hidden" type="checkbox" name="chip-1" checked>
-                      <span class="visually-hidden">Вернуть чип на склад</span>
-                    </label>
-                  </li>
-                  <li>
-                    <label class="components__item components__item--chip">
-                      <svg class="components__logo" width="24" height="24">
-                        <use href="@/assets/img/sprite.svg#chip"></use>
-                      </svg>
-                      <input class="visually-hidden" type="checkbox" name="chip-2">
-                      <span class="visually-hidden">Добавить чип  У вас есть: 1</span>
-                    </label>
-                  </li>
-                  <li>
-                    <label class="components__item components__item--chip">
-                      <svg class="components__logo" width="24" height="24">
-                        <use href="@/assets/img/sprite.svg#chip"></use>
-                      </svg>
-                      <input class="visually-hidden" type="checkbox" name="chip-3" disabled>
-                    </label>
-                  </li>
-                  <li>
-                    <label class="components__item components__item--chip">
-                      <svg class="components__logo" width="24" height="24">
-                        <use href="@/assets/img/sprite.svg#chip"></use>
-                      </svg>
-                      <input class="visually-hidden" type="checkbox" name="chip-4" disabled>
-                    </label>
-                  </li>
-                </ul>
-                <ul class="components__list">
-                  <li>
-                    <label class="components__item components__item--soul">
-                      <svg class="components__logo" width="32" height="32">
-                        <use href="@/assets/img/sprite.svg#soul"></use>
-                      </svg>
-                      <input class="visually-hidden" type="checkbox" name="soul">
-                      <span class="visually-hidden">Добавить душу. У вас есть: 1</span>
-                    </label>
-                  </li>
-                </ul>
-              </fieldset>
-            </div>
-            <div class="constructor__info">
-              <AppButton class="constructor__button button--red-border" type="submit" disabled>Произвести за 10 монет</AppButton>
-              <p class="constructor__description">Для производства биоробота не хватает 2 биоруки, 3 микрочипа и 1 души</p>
-            </div>
-          </form>
-          <div class="constructor__image-wrapper">
-            <img class="constructor__robot" src="@/assets/img/robots/robot-frontend-male-unavailable.png" alt="Твой робот. Статус: недоступен для производства" width="236" height="320">
+      <span
+        class="installation__number section-number section-number--installation"
+      >05</span>
+      <h2 class="installation__title section-title">
+        Производство
+      </h2>
+      <form
+        id="constructor"
+        class="installation__constructor constructor"
+        method="POST"
+        action="#"
+      >
+        <div class="constructor__body">
+          <div class="constructor__settings">
+            <fieldset
+              v-for="parameter in dataStore.robotParameters"
+              :key="parameter.id"
+              class="constructor__parameter parameter"
+            >
+              <legend class="parameter__title">
+                {{ parameter.title }}
+              </legend>
+              <AppRadio
+                v-for="variant in parameter.variants"
+                :key="variant.value"
+                v-model:link="robotStore[parameter.name]"
+                class="parameter__label"
+                :name="parameter.name"
+                :value="variant.value"
+                :data-test="robotStore.stabilizer.value"
+              >
+                {{ variant.description }}
+              </AppRadio>
+            </fieldset>
           </div>
+          <fieldset class="constructor__components components">
+            <legend class="visually-hidden">
+              Добавить компоненты для производства
+            </legend>
+            <ul
+              v-for="component in dataStore.components"
+              :key="component.id"
+              class="components__list"
+            >
+              <li
+                v-for="(element, index) in component.requiredQuantity"
+                :key="index"
+              >
+                <label
+                  :class="`components__item components__item--${component.value}`"
+                >
+                  <svg class="components__logo">
+                    <use
+                      :href="`/sprite.svg#${component.value}`"
+                    />
+                  </svg>
+                  <input
+                    class="visually-hidden"
+                    type="checkbox"
+                    :data-component-id="component.id"
+                    :checked="
+                      getStartQuantity(component.id) > index
+                        && componentsStore.getComponentQuantity(component.id)
+                    "
+                    :disabled="
+                      index + 1
+                        > componentsStore.getComponentQuantity(component.id)
+                    "
+                    @change="manageComponents"
+                  />
+                  <span class="visually-hidden">Вернуть компонент {{ component.name }} на склад</span>
+                </label>
+              </li>
+            </ul>
+          </fieldset>
         </div>
+        <div class="constructor__info">
+          <div class="constructor__button-wrapper">
+            <AppButton
+              class="constructor__button button--red-border"
+              type="submit"
+              :disabled="!robotStore.canProduce"
+              @click.prevent="robotStore.isProduced = true"
+            >
+              Произвести за {{ robotStore.productionCost }} монет
+            </AppButton>
+          </div>
+          <p v-if="!robotStore.canProduce" class="constructor__description">
+            {{ robotStore.robotStatusDescription }}
+          </p>
+        </div>
+        <div class="constructor__image-wrapper">
+          <div
+            class="constructor__robot"
+            :class="robotStore.robotClass"
+            role="img"
+            :aria-label="`Твой робот. Статус: ${robotStore.isProduced ? 'Произведен' : robotStore.robotStatus.title}`"
+          />
+        </div>
+      </form>
     </div>
   </section>
 </template>
 
+<script setup>
+import { useDataStore } from '../store/dataStore';
+import { useComponentsStore } from '../store/componentsStore';
+import { useRobotStore } from '../store/robotStore';
+
+const dataStore = useDataStore();
+const robotStore = useRobotStore();
+const componentsStore = useComponentsStore();
+
+// Удаляем реактивность, чтобы активные элементы отображались корректно
+
+const startComponentsQuantity = robotStore.installedComponents.map(
+  (component) => ({
+    id: component.id,
+    quantity: component.quantity,
+  }),
+);
+const getStartQuantity = (componentId) => (
+  startComponentsQuantity.find((component) => component.id === componentId)
+    ?.quantity || 0
+);
+
+const manageComponents = (event) => {
+  if (event.target.checked) {
+    robotStore.setComponent(event.target.dataset.componentId);
+  } else {
+    robotStore.removeComponent(event.target.dataset.componentId);
+  }
+};
+</script>
+
 <style lang="scss" scoped>
-@import "@/assets/scss/text-style.scss";
-@import "@/assets/scss/device-width.scss";
-@import "@/assets/scss/colors.scss";
+@import "../assets/scss/text-style.scss";
+@import "../assets/scss/device-width.scss";
+@import "../assets/scss/colors.scss";
 
 .installation {
   padding-bottom: 205px;
@@ -136,67 +149,58 @@
   line-height: 72px;
 }
 
-.installation__constructor {
-  width: 756px;
-
-  @media(max-width: $tablet-max) {
-    width: 496px;
-  }
-
-  @media(max-width: 650px) {
-    width: 236px;
-  }
-}
-
-// Конструктор
-
 .constructor {
   display: grid;
-  grid-template-columns: 482px 236px;
-  column-gap: 38px;
+  grid-template-columns: 2fr 1fr;
+  grid-template-rows: auto auto;
+  grid-template-areas:
+    "body preview"
+    "info preview";
+  column-gap: 39px;
+  width: 73px;
+  min-width: 756px;
+  margin: 0 auto;
 
-  @media(max-width: 650px) {
-    grid-template-columns: 234px 236px;
-    column-gap: 28px;
+  @media (max-width: $tablet-max) {
+    grid-template-columns: 1.1fr 1fr;
+    row-gap: 20px;
+    min-width: 496px;
   }
 
-  @media(max-width: $phone-max) {
-    display: flex;
-    flex-direction: column-reverse;
-    margin: 0 auto;
+  @media (max-width: $phone-max) {
+    grid-template-columns: 1fr;
+    grid-template-rows: repeat(3, auto);
+    grid-template-areas:
+      "preview"
+      "body"
+      "info";
+    min-width: 236px;
   }
-}
-
-.constructor__form {
-  display: flex;
-  flex-direction: column;
 }
 
 .constructor__body {
   display: grid;
-  grid-template-columns: 229px 222px;
-  column-gap: 28px;
-  margin-bottom: 61px;
+  grid-template-columns: 1.1fr 1fr;
+  column-gap: 24px;
+  margin-bottom: 32px;
 
-  @media(max-width: 650px) {
+  @media (max-width: $tablet-max) {
     display: flex;
     flex-direction: column;
-    margin-bottom: 20px;
+    row-gap: 44px;
   }
 }
 
 .constructor__settings {
-  @include second-text;
   display: flex;
   flex-direction: column;
-  row-gap: 36px;
-
-  @media(max-width: 650px) {
-    margin-bottom: 44px;
-  }
+  row-gap: 40px;
 }
 
 .parameter {
+  @include second-text;
+  display: flex;
+  column-gap: 24px;
   margin: 0;
   padding: 0px;
   border: none;
@@ -208,11 +212,100 @@
   padding: 0;
 }
 
-.parameter__wrapper {
-  display: grid;
-  grid-template-columns: repeat(2, 99px);
-  gap: 32px;
+.parameter__label {
+  width: 100px;
 }
+
+.constructor__info {
+  grid-area: info;
+  display: grid;
+  grid-template-columns: 1.1fr 1fr;
+  column-gap: 24px;
+
+  @media (max-width: $tablet-max) {
+    display: flex;
+    flex-direction: column-reverse;
+    row-gap: 20px;
+  }
+}
+
+.constructor__button {
+  min-width: 236px;
+}
+
+.constructor__description {
+  @include second-text;
+  width: 222px;
+  margin: 0;
+}
+
+.constructor__image-wrapper {
+  grid-area: preview;
+  max-height: 320px;
+}
+
+.constructor__robot {
+  width: 236px;
+  height: 320px;
+  background-image: url("../assets/img/robots-sprite.png");
+  background-repeat: no-repeat;
+  transition-duration: 200ms;
+  transition-timing-function: ease-in-out;
+
+  @media (min-resolution: 2dppx) {
+    background-image: url("../assets/img/robots-sprite@2x.png");
+  }
+
+  &--frontend-male-produced {
+    background-position: 0 0;
+  }
+
+  &--design-male-produced {
+    background-position: -236px 0;
+  }
+
+  &--frontend-female-produced {
+    background-position: -472px 0;
+  }
+
+  &--design-female-produced {
+    background-position: -708px 0;
+  }
+
+  &--frontend-male-available {
+    background-position: 0 -320px;
+  }
+
+  &--design-male-available {
+    background-position: -236px -320px;
+  }
+
+  &--frontend-female-available {
+    background-position: -472px -320px;
+  }
+
+  &--design-female-available {
+    background-position: -708px -320px;
+  }
+
+  &--frontend-male-unavailable {
+    background-position: 0 -640px;
+  }
+
+  &--design-male-unavailable {
+    background-position: -236px -640px;
+  }
+
+  &--frontend-female-unavailable {
+    background-position: -472px -640px;
+  }
+
+  &--design-female-unavailable {
+    background-position: -708px -640px;
+  }
+}
+
+// Компоненты
 
 .components {
   margin: 0;
@@ -241,6 +334,28 @@
   height: 48px;
   color: $white;
   background-color: $dark-gray;
+  transition-duration: 200ms;
+  transition-timing-function: ease-in-out;
+
+  & svg {
+    width: 32px;
+    height: 32px;
+  }
+
+  &--biohand svg {
+    width: 32px;
+    height: 24px;
+  }
+
+  &--chip svg {
+    width: 24px;
+    height: 24px;
+  }
+
+  &--soul svg {
+    width: 32px;
+    height: 32px;
+  }
 
   &:has(input:checked) {
     color: $orange;
@@ -251,87 +366,9 @@
 
   &:has(input:disabled) {
     color: $gray;
+    border-radius: 0;
+    border-color: transparent;
+    background-color: $dark-gray;
   }
-}
-
-.constructor__info {
-  display: flex;
-  align-items: start;
-  column-gap: 28px;
-
-  @media(max-width: 650px) {
-    flex-direction: column-reverse;
-  }
-}
-
-.constructor__button {
-  min-width: 236px;
-}
-
-.constructor__description {
-  @include second-text;
-  width: 222px;
-  margin: 0;
-
-  @media(max-width: 650px) {
-    margin-bottom: 20px;
-  }
-}
-
-.constructor__image-wrapper {
-  @media(max-width: $phone-max) {
-    width: 236px;
-    margin: 0 auto;
-    margin-bottom: 30px;
-  }
-}
-
-.constructor__image-wrapper img {
-  object-fit: contain;
-  width: 100%;
-  height: 100%;
-}
-
-// radio
-
-.radio {
-  display: flex;
-  align-items: center;
-  position: relative;
-  padding-left: 36px;
-  cursor: pointer;
-}
-
-.radio::before {
-  content: "";
-  position: absolute;
-  top: 50%;
-  left: 0;
-  transform: translateY(-50%);
-  width: 24px;
-  height: 24px;
-  border: 2px solid $light-blue;
-  border-radius: 50%;
-}
-
-.radio::after {
-  content: "";
-  display: none;
-  position: absolute;
-  top: 50%;
-  left: 4px;
-  transform: translateY(-50%);
-  width: 16px;
-  height: 16px;
-  background-color: $orange;
-  border-radius: 50%;
-}
-
-.radio:has(.radio__input:checked)::after {
-  display: block;
-}
-
-.radio__text {
-  margin: 0;
 }
 </style>
---
+++ src/store/robotStore.js
@@ -0,0 +1,157 @@
+/* eslint-disable import/prefer-default-export */
+
+import { ref, reactive, computed } from 'vue';
+import { defineStore } from 'pinia';
+import { useDataStore } from './dataStore';
+import { useCoinsStore } from './coinsStore';
+
+import { getComponentDescription } from '../common/helpers';
+
+export const useRobotStore = defineStore('robotStore', () => {
+  const type = ref('design');
+  const stabilizer = ref('male');
+  const installedComponents = reactive([]);
+  const productionCost = ref(10);
+  const isProduced = ref(false);
+
+  const allComponentsInstalled = computed(() => {
+    const dataStore = useDataStore();
+    return !dataStore.components.filter(
+      (component) => component.requiredQuantity > getComponentQuantity(component.id),
+    ).length;
+  });
+
+  const canProduce = computed(() => {
+    const coinsStore = useCoinsStore();
+    return coinsStore.coinsBalance >= 10 && allComponentsInstalled.value;
+  });
+
+  const robotStatus = computed(() => {
+    if (canProduce.value) {
+      return {
+        value: 'available',
+        title: 'Доступен для производства',
+      };
+    }
+    return {
+      value: 'unavailable',
+      title: 'Недоступен для производства',
+    };
+  });
+
+  const robotStatusDescription = computed(() => {
+    const dataStore = useDataStore();
+    const coinsStore = useCoinsStore();
+
+    let finalString = 'Для производства биоробота не хватает';
+    const requiredComponents = dataStore.components
+      .filter(
+        (component) => component.requiredQuantity > getComponentQuantity(component.id),
+      )
+      .map((component) => ({
+        id: component.id,
+        quantity:
+            component.requiredQuantity - getComponentQuantity(component.id),
+      }));
+
+    if (coinsStore.coinsBalance < productionCost.value) {
+      requiredComponents.push({
+        id: 'coins',
+      });
+    }
+
+    if (installedComponents.length === 0) {
+      requiredComponents.pop();
+      requiredComponents.forEach((component, index) => {
+        let currentComponentString = '';
+        if (index + 1 === requiredComponents.length && requiredComponents.length > 1) {
+          currentComponentString += ' и';
+        }
+
+        if (index + 1 === requiredComponents.length && requiredComponents.length === 1) {
+          currentComponentString += ' ';
+        }
+
+        if (index + 1 === requiredComponents.length - 1 && requiredComponents.length > 1) {
+          currentComponentString += ` ${getComponentDescription(component.id, 0)}`;
+        } else if (index + 1 === requiredComponents.length && requiredComponents.length > 1) {
+          currentComponentString += ` ${getComponentDescription(component.id, 0)}`;
+        } else {
+          currentComponentString += ` ${getComponentDescription(component.id, 0)},`;
+        }
+
+        finalString += currentComponentString;
+      });
+    } else {
+      requiredComponents.forEach((component, index) => {
+        let currentComponentString = '';
+        if (index + 1 === requiredComponents.length && requiredComponents.length > 1) {
+          currentComponentString += ' и';
+        }
+
+        if (index + 1 === requiredComponents.length && requiredComponents.length === 1) {
+          currentComponentString += ' ';
+        }
+
+        if (index + 1 === requiredComponents.length - 1 && requiredComponents.length > 1) {
+          currentComponentString += ` ${component.quantity} ${getComponentDescription(component.id, component.quantity)}`;
+        } else if (index + 1 === requiredComponents.length && requiredComponents.length > 1) {
+          currentComponentString += component.id === 'coins' ? ' денег' : ` ${component.quantity} ${getComponentDescription(component.id, component.quantity)}`;
+        } else {
+          currentComponentString += component.id === 'coins' ? ' денег' : ` ${component.quantity} ${getComponentDescription(component.id, component.quantity)},`;
+        }
+
+        finalString += currentComponentString;
+      });
+    }
+
+    return finalString;
+  });
+
+  const robotClass = computed(() => `constructor__robot--${type.value}-${stabilizer.value}-${isProduced.value ? 'produced' : robotStatus.value.value}`);
+
+  const getComponentQuantity = (componentId) => (
+    installedComponents.find((component) => component.id === componentId)
+      ?.quantity || 0
+  );
+
+  const setComponent = (componentId) => {
+    const currentComponentIndex = installedComponents.findIndex(
+      (component) => component.id === componentId,
+    );
+    if (currentComponentIndex !== -1) {
+      installedComponents[currentComponentIndex].quantity += 1;
+    } else {
+      installedComponents.push({
+        id: componentId,
+        quantity: 1,
+      });
+    }
+  };
+
+  const removeComponent = (componentId) => {
+    const currentComponentIndex = installedComponents.findIndex(
+      (component) => component.id === componentId,
+    );
+    if (installedComponents[currentComponentIndex].quantity - 1 > 0) {
+      installedComponents[currentComponentIndex].quantity -= 1;
+    } else {
+      installedComponents.splice(currentComponentIndex, 1);
+    }
+  };
+
+  return {
+    type,
+    stabilizer,
+    installedComponents,
+    productionCost,
+    isProduced,
+    canProduce,
+    robotStatus,
+    robotClass,
+    robotStatusDescription,
+    getComponentQuantity,
+    setComponent,
+    removeComponent,
+  };
+});
